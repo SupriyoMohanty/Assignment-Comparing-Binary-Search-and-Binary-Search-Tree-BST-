37f489402bdf93684a9f94fb600cab1c76c3c0fd
SupriyoMohanty <111578161+SupriyoMohanty@users.noreply.github.com>
2022-11-20 11:36:03 +0530

    Add files via upload

---
+++ Assingment_21105124_SupriyoMohanty.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# Binary Search Tree operations in Python 
+
+
+# Create a node
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+# Inorder traversal
+def inorder(root):
+    if root is not None:
+        # Traverse left
+        inorder(root.left)
+
+        # Traverse root
+        print(str(root.key) + "->", end=' ')
+
+        # Traverse right
+        inorder(root.right)
+
+
+# Insert a node
+def insert(node, key):
+
+    # Return a new node if the tree is empty
+    if node is None:
+        return Node(key)
+
+    # Traverse to the right place and insert the node
+    if key < node.key:
+        node.left = insert(node.left, key)
+    else:
+        node.right = insert(node.right, key)
+
+    return node
+
+
+# Find the inorder successor
+def minValueNode(node):
+    current = node
+
+    # Find the leftmost leaf
+    while(current.left is not None):
+        current = current.left
+
+    return current
+
+
+# Deleting a node
+def deleteNode(root, key):
+
+    # Return if the tree is empty
+    if root is None:
+        return root
+
+    # Find the node to be deleted
+    if key < root.key:
+        root.left = deleteNode(root.left, key)
+    elif(key > root.key):
+        root.right = deleteNode(root.right, key)
+    else:
+        # If the node is with only one child or no child
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+
+        # If the node has two children,
+        # place the inorder successor in position of the node to be deleted
+        temp = minValueNode(root.right)
+
+        root.key = temp.key
+
+        # Delete the inorder successor
+        root.right = deleteNode(root.right, temp.key)
+
+    return root
+arr=[8,3,1,6,7,10,14,4]
+n=len(arr)
+print("Array of numbers:",arr)
+root = None
+root = insert(root, 8)
+root = insert(root, 3)
+root = insert(root, 1)
+root = insert(root, 6)
+root = insert(root, 7)
+root = insert(root, 10)
+root = insert(root, 14)
+root = insert(root, 4)
+
+print("Inorder traversal for binary tree: ", end=' ')
+inorder(root)
+
+print("\nDelete 10")
+root = deleteNode(root, 10)
+print("\nInorder traversal for updated binary tree: ", end=' ')
+print(inorder(root))
+
+
+# In[44]:
+
+
+# Iterative Binary Search Function for finding and deleting the number
+# It returns index of number to be deleted in given array arr if present,
+# else returns -1
+def binary_search(arr, x):
+	low = 0
+	high = len(arr) - 1
+	mid = 0
+
+	while low <= high:
+
+		mid = (high + low) // 2
+
+		# If x is greater, ignore left half
+		if arr[mid] < x:
+			low = mid + 1
+
+		# If x is smaller, ignore right half
+		elif arr[mid] > x:
+			high = mid - 1
+
+		# means x is present at mid
+		else:
+			return mid
+
+	# If we reach here, then the element was not present
+	return -1
+
+
+# Test array
+arr =  [8,3,1,6,7,10,14,4]
+x = 14
+
+# Function call
+result = binary_search(arr, x)
+
+if result != -1:
+	print("Element is present at index", str(result))
+else:
+	print("Element is not present in array")
+arr.pop(result) #deleting element
+print(arr)
+
+
+# ### Space complexity of a "binary search tree on average is O(logn)" as on average there are at most logn calls in the recursion stack. In the "worst case, space complexity can be O(n)". The space complexity of "binary search in the iterative method" is O(1).
+# 
+# # So Binary search is better than Binary Search Tree in terms of space complexity
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
